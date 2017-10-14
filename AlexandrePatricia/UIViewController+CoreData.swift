ec887c6968fdd37725214c83ae5c14a85abf6083
ABC Education <abceducation@AirAbc.local>
2017-10-13 22:10:59 -0300

    tela de ajustes inicio add estado

---
+++ AlexandrePatricia/UIViewController+CoreData.swift
@@ -0,0 +1,12 @@
+import UIKit
+import CoreData
+
+
+extension UIViewController {
+    var appDelegate: AppDelegate {
+        return UIApplication.shared.delegate as! AppDelegate
+    }
+    var context: NSManagedObjectContext {
+        return appDelegate.persistentContainer.viewContext
+    }
+}
